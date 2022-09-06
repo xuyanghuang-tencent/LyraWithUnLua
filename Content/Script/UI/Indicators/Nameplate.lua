183aeb7d9a9269e180ab58d2ed07c71879b0aa3e
xuyanghuang <xuyanghuang@tencent.com>
2022-09-06 20:40:37 +0800

    增加：玩家头顶姓名板UI的覆写

---
+++ Content/Script/UI/Indicators/Nameplate.lua
@@ -0,0 +1,82 @@
+---@type W_Nameplate_C
+local M = UnLua.Class()
+
+function M:PawnReady()
+    local Name = self.Pawn.PlayerState:GetPlayerName()
+    self:InitializeNameplate(self.Pawn, Name)
+end
+
+function M:InitializeNameplate(Pawn, Name)
+    self.Pawn = Pawn
+    self.NameText = Name
+    self.Indicator:SetDesiredVisibility(true)
+    self:SetVisibility(UE.ESlateVisibility.Visible)
+
+    -- Player Name
+    self:SetNameText(self.NameText)
+
+    -- Team Colors
+    local AsyncAction = UE.UAsyncAction_ObserveTeam.ObserveTeam(self.Pawn)
+    AsyncAction.OnTeamChanged:Add(self, function(_, bTeamSet, TeamId)
+        if not bTeamSet then
+            return
+        end
+        self.TeamId = TeamId
+        self:SetTeamVisuals()
+    end)
+    AsyncAction:Activate()
+
+    -- Health
+    local HealthComponent = UE.ULyraHealthComponent.FindHealthComponent(self.Pawn)
+    if HealthComponent then
+        HealthComponent.OnHealthChanged:Add(self, self["Health Changed"])
+        HealthComponent.OnDeathStarted:Add(self, self.DeathStarted)
+        self:SetHealthBarPercent(HealthComponent:GetHealthNormalized())
+    end
+
+    self:UpdateVisibility()
+end
+
+M["Health Changed"] = function(self, HealthComponent, OldValue, NewValue, Instigator)
+    local HealthPercent = HealthComponent:GetHealthNormalized()
+    self:SetHealthBarPercent(HealthPercent)
+end
+
+function M:SetHealthBarPercent(HealthPercent)
+    self.Health:GetDynamicMaterial():SetScalarParameterValue("Progress", HealthPercent)
+end
+
+function M:DeathStarted(OwningActor)
+    self.Indicator:UnregisterIndicator()
+end
+
+function M:SetNameText(PlayerName)
+    self.Name:SetText(PlayerName)
+end
+
+function M:BindIndicator(Indicator)
+    self.Indicator = Indicator
+    self.Pawn = Indicator:GetDataObject():Cast(UE.APawn)
+    Indicator:SetDesiredVisibility(false)
+    local PawnExtensionComponent = UE.ULyraPawnExtensionComponent.FindPawnExtensionComponent(self.Pawn)
+    if PawnExtensionComponent:IsPawnReadyToInitialize() then
+        self:PawnReady()
+    else
+        PawnExtensionComponent.BP_OnPawnReadyToInitialize:Add(self, self.PawnReady)
+    end
+end
+
+function M:UnbindIndicator(Indicator)
+    local HealthComponent = UE.ULyraHealthComponent.FindHealthComponent(self.Pawn)
+    if HealthComponent then
+        HealthComponent.OnHealthChanged:Remove(self, self["Health Changed"])
+        HealthComponent.OnDeathStarted:Remove(self, self.DeathStarted)
+    end
+
+    local PawnExtensionComponent = UE.ULyraPawnExtensionComponent.FindPawnExtensionComponent(self.Pawn)
+    PawnExtensionComponent.BP_OnPawnReadyToInitialize:Remove(self, self.PawnReady)
+    self.Pawn = nil
+    self.Indicator = nil
+end
+
+return M
